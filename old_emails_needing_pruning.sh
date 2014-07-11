355c6276a905d77bcfd2158e3d8ec9528d60c102
deoren <deoren@users.noreply.github.com>
2014-07-11 06:33:32 +0000

    Rough script to display emails in Junk/Spam/Trash folders that are good candidates for pruning.

---
+++ old_emails_needing_pruning.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# $Id$
+# $HeadURL$
+
+# Purpose: Display emails older than X days in the Trash
+
+# Dovecot calls these 'mailboxes', so I used the same terminology here.
+mailboxes=(
+    Trash
+    Spam
+    Junk
+)
+
+cutoff_date="50days"
+
+echo -e "\n#################################################################"
+echo "Emails in Trash/Junk/Spam folders older than ${cutoff_date}"
+echo -e "#################################################################\n"
+
+for mailbox in "${mailboxes[@]}"
+do
+    for account in $(
+
+        doveadm -v search -A mailbox ${mailbox} before ${cutoff_date} \
+            | cut -f 1 -d ' ' \
+            | sort \
+            | uniq
+    )
+        do echo "${account} - ${mailbox}: $(doveadm -v search -u ${account} mailbox ${mailbox} before ${cutoff_date} | wc -l)"
+    done
+done
