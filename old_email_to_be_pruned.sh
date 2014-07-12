fa8e0c8db0ff613bb8f8296c7c2094a2771bd992
deoren <deoren@users.noreply.github.com>
2014-07-12 07:05:07 +0000

    * Added functions to help shrink main body of code (still crude, but I'm too lazy at the moment to add lots of polish).

--- old_email_to_be_pruned.sh
+++ old_email_to_be_pruned.sh
@@ -15,6 +15,47 @@ mailboxes_to_prune=(
 
 cutoff_date="60days"
 
+#
+# Functions
+#
+
+get_accounts_with_old_mail () {
+
+    doveadm -v search -A mailbox ${mailbox} before ${cutoff_date} \
+        | cut -f 1 -d ' ' \
+        | sort \
+        | uniq
+
+}
+
+print_mailbox_match_count () {
+
+    account=$1
+    mailbox=$2
+
+    msg_match_count=$(doveadm -v search -u ${account} mailbox ${mailbox} before ${cutoff_date} | wc -l)
+
+    echo -e "\n${account} [${mailbox}]: ${msg_match_count}"
+
+}
+
+print_mailbox_match_subject_lines () {
+
+    account=$1
+    mailbox=$2
+
+    doveadm search -u ${account} mailbox ${mailbox} before ${cutoff_date} | 
+    while read guid uid
+    do 
+        doveadm fetch -u ${account} hdr mailbox-guid $guid uid $uid | grep -i 'Subject: '
+    done
+
+}
+
+#
+# Main code body
+#
+
 echo -e "\n#################################################################"
 echo -e "Emails older than ${cutoff_date} in these folders:"
 
@@ -25,13 +66,11 @@ echo -e "#################################################################\n"
 
 for mailbox in "${mailboxes_to_prune[@]}"
 do
-    for account in $(
 
-        doveadm -v search -A mailbox ${mailbox} before ${cutoff_date} \
-            | cut -f 1 -d ' ' \
-            | sort \
-            | uniq
-    )
-        do echo "${account} - ${mailbox}: $(doveadm -v search -u ${account} mailbox ${mailbox} before ${cutoff_date} | wc -l)"
+    for account in $(get_accounts_with_old_mail)
+        do 
+            print_mailbox_match_count "${account}" "${mailbox}"
+            echo "---------------------------------------------------"
+            print_mailbox_match_subject_lines "${account}" "${mailbox}"
     done
 done
