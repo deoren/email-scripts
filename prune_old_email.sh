c36f6d304e34c199164de42d1679943d131db157
deoren <deoren@users.noreply.github.com>
2014-08-22 03:46:31 +0000

    Reverted flag change that I accidentally flipped in the last revision.

--- prune_old_email.sh
+++ prune_old_email.sh
@@ -34,7 +34,7 @@
 #############################################################################
 
 # Turn this off if testing other portions of the code (reporting for example)
-EMAIL_PRUNING_ENABLED=0
+EMAIL_PRUNING_ENABLED=1
 
 # Verbose output. Useful to verify that the script works as a whole. Recommended
 # to disable if not actively testing/verifying script functionality.
@@ -50,7 +50,7 @@ DISPLAY_PRUNING_OUTPUT=1
 #
 
 # Displays the subject lines of emails that are about to be removed
-DISPLAY_EMAIL_SUBJECT_LINES=0
+DISPLAY_EMAIL_SUBJECT_LINES=1
 
 # Displays the account, mailbox and number of emails that are about to be 
 # removed from it
